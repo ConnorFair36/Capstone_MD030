4bf027be11ae2befadb10768da3a65d868223f86
Connor Fair <faircw@egr-u-ec-ym-04.rams.adp.vcu.edu>
2026-03-25 14:21:31 -0400

    Put the model on the GPU

--- src/Python/Machine_Learning/Center_Fusion/main.py
+++ src/Python/Machine_Learning/Center_Fusion/main.py
@@ -6,6 +6,10 @@ from model.model import getModel, loadModel
 from config.utils import updateDatasetAndModelConfig
 from config.default import _Cfg
 
+from model.decode import fusionDecode
+
+import time
+
 config_file = "centerfusion_debug.yaml"
 
 # fake dataset, lol
@@ -25,13 +29,37 @@ cf_model = getModel(cfg)
 # load in the model weights
 cf_model = loadModel(cf_model, cfg)[1]
 
+print(torch.cuda.is_available())
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+cf_model = cf_model.to(device)
 
-print("Done!")
-# TODO Figure out what tensor input sizes the model takes
-dummy = torch.zeros(1, 3, 448, 800)
+B = 2
+H, W = 448, 800
+outH, outW = 112, 200  # INPUT_SIZE // 4
+pc_channels = 3
+
+dummy_image = torch.zeros(B, 3, H, W).to(device)           # RGB image
+dummy_pc_hm = torch.zeros(B, pc_channels, outH, outW).to(device)     # radar heatmap at output resolution
+dummy_pc_dep = torch.zeros(B, pc_channels, outH, outW).to(device)    # radar depth at output resolution
+dummy_calib = torch.zeros(B, 3, 4).to(device)              # 3x4 camera intrinsic matrix
+
 cf_model.eval()
 with torch.no_grad():
-    print((cf_model(dummy, pc_hm=dummy, pc_dep=dummy, calib=dummy)).shape)
-
+    for i in range(5):
+        start = time.monotonic()
+        output = cf_model(
+            dummy_image,
+            pc_hm=dummy_pc_hm,
+            pc_dep=dummy_pc_dep,
+            calib=dummy_calib
+        )
+        boxes = fusionDecode(output)
+        print(f"Runtime: {time.monotonic() - start}")
+        if i == 4:
+            for key, value in boxes.items():
+                print(f"{key}: {value.shape}")
+            print("----------------")
+            for key, value in output[0].items():
+                print(f"{key}: {value.shape}")
 #for name, param in cf_model.named_parameters():
 #    print(name, param)
